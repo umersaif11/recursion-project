3f84960546dfa02568775f4e6c0aac15c68286fc
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-09-20 16:25:01 +0500

    If n is equal to 0 then push 0 into array

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,10 @@
 function fibsIterative(n){
     let array = [];
     for(let i = 0; i < n; i++){
-        
+        if(n === 0){
+            array.push(0);
+        } else if(n === 1){
+            
+        }
     }
 }
