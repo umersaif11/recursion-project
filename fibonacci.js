050b8b52c1623955b4e433f2f8b2644a540d0d02
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-09-20 16:29:07 +0500

    bug is settled after testing- put i in place of n

--- fibonacci.js
+++ fibonacci.js
@@ -1,12 +1,14 @@
 function fibsIterative(n){
     let array = [];
     for(let i = 0; i < n; i++){
-        if(n === 0){
+        if(i === 0){
             array.push(0);
-        } else if(n === 1){
+        } else if(i === 1){
             array.push(1);
         } else{
             array.push(array[i - 1] + array[i - 2]);
         } 
     }
+    return array;
 }
+console.log(fibsIterative(5));
