02c430db52cfdf513e9e1bb8b8b418705cda93bb
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-09-21 17:20:10 +0500

    After getting mid, get the left and right half of the array using slice method

--- mergesort.js
+++ mergesort.js
@@ -1,7 +1,9 @@
 function mergesort(array){
-    if(array.length === 1){
+    if(array.length <= 1){
         return array;
     } else {
-        const mid = Math.floor(array.length / 2)
+        const mid = Math.floor(array.length / 2);
+        const leftHalf = array.slice(0, mid);
+        const rightHalf = array.slice(mid);
     }
 }
